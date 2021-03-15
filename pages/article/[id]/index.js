9b779427e38485a94f568033a62c3e7af9d18688
sajeebmahamed <sajeebmahamed@gmail.com>
2021-03-15 10:19:58 +0000

    getServerSideProps/getStaticPath

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+const articles = ({article}) => {
+    // const router = useRouter()
+    // const {id} = router.query
+    return <div>
+        <h1> { article.title } </h1>
+        <h1> { article.body } </h1>
+        <br />
+        <Link href='/'> Go Back </Link>
+    </div>
+}
+
+// export const getServerSideProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json()
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  
+    const article = await res.json()
+  
+    return {
+      props: {
+        article,
+      },
+    }
+  }
+  
+  export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  
+    const articles = await res.json()
+  
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  
+    return {
+      paths,
+      fallback: false,
+    }
+  }
+
+export default articles
